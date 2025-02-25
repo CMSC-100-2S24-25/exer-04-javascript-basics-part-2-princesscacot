bf3e007a264827c1b0ae97d53a8e86aec6017e6d
princesscacot <pvcacot@up.edu.ph>
2025-02-25 17:18:48 +0800

    fix: added the function addAcount that would add its details in the users.txt when all conditions are true.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import {v4 as uuidv4} from 'uuid';
+import isEmail from 'validator/lib/isEmail.js';
+import { appendFileSync } from 'node:fs';
 
 function generateUniqueID(firstName, lastName){
 
@@ -10,10 +12,38 @@ function generateUniqueID(firstName, lastName){
     const generateUniqueAlphaNum = () => {
         return uuidv4()
     };
+    
     let uniqueAlphaNum = generateUniqueAlphaNum(); // put the generated alphanum
 
     let finalUniqueAlphaNum = uniqueAlphaNum.slice(0,8); // extract 8 alphanum only 
     return generatedName + finalUniqueAlphaNum; //concatenate the generated name with unique alphanum
 }
 
-export{generateUniqueID};
+function addAccount(firstName, lastName, email, age){
+
+    //check requirements 
+    if (
+        typeof firstName === "string" && firstName.trim() !== "" && //check if string (firstname, lastname and email)
+        typeof lastName === "string" && lastName.trim() !== "" && //trim and compare to make sure it is not non emptry string
+        typeof email === "string" && isEmail(email) && // for email used the imported validator
+        typeof age === "number" && age >= 18 // age must be a number and must be at least 18 
+    ) {
+        let uniqueId = generateUniqueID(firstName,lastName);
+        let accDetails = `${firstName}, ${lastName}, ${email}, ${age}, ${uniqueId}`;
+        
+
+        // Append details to users.txt 
+        try {
+            appendFileSync("users.txt", accDetails + "\n"); 
+            console.log("Account successfully added.");
+            return true; 
+        } catch (err) {
+            return false;
+        }
+    } else {
+        console.log("There's an invalid input"); // just to print smth 
+    }
+}
+
+
+export{generateUniqueID, addAccount};
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 import{addAccount, generateUniqueID} from './index.js'
 
-console.log(generateUniqueID("Kel", "GALLEGOS")); 
+console.log(generateUniqueID("Ces", "CACOT")); //test generation of unique id
+console.log(addAccount("Tim", "Berners-Lee", "tim@w3c.com", 25))
